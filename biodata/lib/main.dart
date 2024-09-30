4ca714d11a390ab5d2f45961a4446be836023558
Dayudwi134 <ayudwiwirayanti@gmail.com>
2024-09-30 13:27:59 +0800

    ganti nama

--- biodata/lib/main.dart
+++ biodata/lib/main.dart
@@ -106,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'dayu have pushed the button this many times:',
+              'Dayu Dwi have pushed the button this many times:',
             ),
             Text(
               '$_counter',
